0e578a6ea6a5086a5c9b2575f16f976149a0ac4d
clena <storeclena@gmail.com>
2025-10-18 16:09:25 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,22 @@
-// 🧱 CACHE BÁSICO DO PWA
 const CACHE_NAME = 'mercatto-cache-v1';
 const URLS_TO_CACHE = [
   '/',
   '/index.html',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/manifest.json'
 ];
 
-// Instala e adiciona ao cache
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS_TO_CACHE)));
   self.skipWaiting();
 });
 
-// Ativa e limpa cache antigo
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+  )));
   self.clients.claim();
 });
 
-// Busca offline
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
